d843ab7dad1baf4cfb4ce082cf33814407fe5606
roni <ksri2457@gmail.com>
2025-04-03 16:29:05 +0900

    3-1

---
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Getter
+@Table(name = "user_tb")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String email;
+
+    @CreationTimestamp // now가 자동으로 들어간다
+    private Timestamp createdAt;
+
+    @Builder // 풀생성자 만들고 빌더 어노테이션 붙이기 -> 빌더
+    public User(Integer id, String username, String password, String email, Timestamp createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+}
